e79d4f8969164c621c34f5ab302fc35cf3065c45
Sonukr <sonuyadav9756@gmail.com>
2017-04-19 12:20:29 +0530

    gif item added

--- src/index.js
+++ src/index.js
@@ -2,38 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search';
 import GifList from './components/giflist';
+import request from 'superagent';
+
+
+
 class App extends React.Component {
 	
+		
   handleTermChange(term) {
-		console.log(term);
+  	const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;	
+    request.get(url, (err, res) => {
+      this.setState({ gifs: res.body.data })
+    });
   }
 		
-		 constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      gifs: [
-        {
-          id: 1,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 2,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 3,
-          url: 'http://fakeimg.pl/300/'
-        }
-      ]
+      gifs: [ ]
     }
+		
+		   this.handleTermChange = this.handleTermChange.bind(this);
   }
+		
 		
 	render() {
 		return (
 			<div className="greeting">
 				<p className="greeting-text">Hello World!</p>
-				<SearchBar onTermChange={this.handleTermChange} />
+				<SearchBar onTermChange={term => this.handleTermChange(term)} />
 				<GifList gifs={this.state.gifs} />
 			</div>
 		);
